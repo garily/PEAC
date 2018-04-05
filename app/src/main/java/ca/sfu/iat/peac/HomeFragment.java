41cc58b27ef6ed4e588ab98dc15528684ee7ea40
Gary Li <taichi@starlite.me>
2018-04-05 02:13:59 -0700

    created TestFragment

--- app/src/main/java/ca/sfu/iat/peac/HomeFragment.java
+++ app/src/main/java/ca/sfu/iat/peac/HomeFragment.java
@@ -1,10 +1,16 @@
 package ca.sfu.iat.peac;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import java.util.Date;
 
 public class HomeFragment extends Fragment {
     @Override
@@ -20,5 +26,21 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        ImageButton btnStartTest = getActivity().findViewById(R.id.imgBtnStartSession);
+        btnStartTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle testInfo = new Bundle();
+                testInfo.putLong("date_n_time", new Date().getTime());
+                testInfo.putInt("type", TestFragment.BASELINE);
+
+                getFragmentManager().beginTransaction()
+                        .addToBackStack("home")
+                        .replace(R.id.fgMain, new TestFragment())
+                        .commit();
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/ca/sfu/iat/peac/TestFragment.java
@@ -0,0 +1,128 @@
+package ca.sfu.iat.peac;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link TestFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link TestFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TestFragment extends Fragment {
+    public static final int BASELINE = 0;
+    public static final int AFTERDOSE = 1;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String TEST_TYPE = "test_type";
+    private static final String TEST_TIME = "test_time";
+
+
+    public int getTestType() {
+        return testType;
+    }
+
+    public void setTestType(int testType) {
+        this.testType = testType;
+    }
+
+    public int getTestTime() {
+        return testTime;
+    }
+
+    public void setTestTime(int testTime) {
+        this.testTime = testTime;
+    }
+
+    // TODO: eeg buffer and time interval buffer
+    private int testType;
+    private int testTime;
+    //private eegbuffer
+
+    private OnFragmentInteractionListener mListener;
+
+    public TestFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param testType Parameter 1.
+     * @param testTime Parameter 2.
+     * @return A new instance of fragment TestFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static TestFragment newInstance(int testType, String testTime) {
+        TestFragment fragment = new TestFragment();
+        Bundle args = new Bundle();
+        args.putInt(TEST_TYPE, testType);
+        args.putString(TEST_TIME, testTime);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            testType = getArguments().getInt(TEST_TYPE);
+            testTime = getArguments().getInt(TEST_TIME);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_test, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
